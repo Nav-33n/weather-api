d96c05406a6450ce24a7028b0cd3868cad88940e
Nabin Sharma <sharmanavin611@gmail.com>
2025-06-08 14:01:18 +0530

    clean up and fix the error

--- server.js
+++ server.js
@@ -1,43 +1,3 @@
-// const PORT = 5670;
-// const express = require('express')
-// const cors = require('cors')
-// require('dotenv').config()
-// const axios = require('axios')
-// const app = express();
-// const bodyParser = require('body-parser');
-
-// app.use(cors())
-// app.use(bodyParser.json());
-
-// app.get('/forecast', async (req, res) => {
-
-//     const timeZoneoffset = Intl.DateTimeFormat().resolvedOptions().timeZone;
-
-//     function getValueAfterSlash(string) {
-//         const match = string.match(/\/([^/]+)$/);
-//         if (match) {
-//           return match[1];
-//         } else {
-//           return null;
-//         }
-//       }
-
-//       let place = req.query.city;
-
-//       if(place == undefined){
-//         place = getValueAfterSlash(timeZoneoffset);
-//       }
-
-//     try {
-//         const response = await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_WEATHER_API_KEY}&q=${place}&days=3&aqi=no&alerts=no`)
-//         res.json(response.data);
-//     } catch (error) {
-//         res.status(500).json({error: 'An error Occurred'});
-//     }
-// });
-
-// app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`))
-
 
 const express = require('express');
 const cors = require('cors');
@@ -78,7 +38,6 @@ app.get('/forecast', async (req, res) => {
     };
 
     const { data } = await axios.get(url, { params });
-    console.log('Weather API response:', data);
     res.json(data);
   } catch (error) {
     console.error('Weather API error:', error.message);
